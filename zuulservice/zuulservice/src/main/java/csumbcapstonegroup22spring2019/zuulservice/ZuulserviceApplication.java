2535e17c556a6d0c20168ba8fdf0045ab083f616
Erik Ring-Walters <erik.ring.walters@gmail.com>
2019-03-30 00:29:46 -0700

    initial commit

---
+++ zuulservice/zuulservice/src/main/java/csumbcapstonegroup22spring2019/zuulservice/ZuulserviceApplication.java
@@ -0,0 +1,13 @@
+package csumbcapstonegroup22spring2019.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulserviceApplication.class, args);
+	}
+
+}
